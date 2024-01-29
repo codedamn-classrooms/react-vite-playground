a623291b15b4da8eacfc5e391f9ae63d1319795e
shiv_bilgundi <shivbilgundiyt@gmail.com>
2024-01-29 15:06:25 +0530

    step2 component updated

--- src/components/OTPAuthentication.jsx
+++ src/components/OTPAuthentication.jsx
@@ -1,11 +1,5 @@
 const OTPAuthentication = () => {
-  return (
-    <div>
-      <p>
-        Lets design a button that produces a random 4-digit number as an OTP.
-      </p>
-    </div>
-  );
+  return <div>OTPAuthentication</div>;
 };
 
 export default OTPAuthentication;
